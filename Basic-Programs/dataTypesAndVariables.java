a971f76b6658d40ab5c46abeec7a8534d934f952
Pushpanjali SK <pushpanjali.Kharatmol@gmail.com>
2023-02-16 20:14:49 +0530

    adding basic programs

---
+++ Basic-Programs/dataTypesAndVariables.java
@@ -0,0 +1,15 @@
+public class dataTypesAndVariables {
+    public static void main(String[] args) {
+        //initialising inteeger variable
+        int num = 10;
+        System.out.println("Value of num: " + num);
+        //reassigning the value
+        num = 20;
+        System.out.println("Value of num: " + num);
+
+        //char data type
+        char letter = 'a';
+        int amount = 10052;
+        
+    }
+}
